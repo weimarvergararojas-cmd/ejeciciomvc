91619a28452d6ad247a6366e321dabeed78111b4
weimarvergararojas-cmd <weimarvergararojas@gmail.com>
2026-07-14 21:03:40 -0500

    se agrego metodo para registro de cliente

--- controlador.js
+++ controlador.js
@@ -1,5 +1,5 @@
 // zona de codigo principal
-function hacer_bucle(){
+/*function hacer_bucle(){
     for (let i = 0; i < 10000; i++) {
     console.log(i);
     }
@@ -27,4 +27,21 @@ boton_enviar.addEventListener("click", ()=>{
     localStorage.setItem("info_producto", JSON.stringify([]));
 
     hacer_saludo();
+});*/
+import ClienteModelo from "./cliente_modelo.js";
+const boton_registrar= document.getElementById("boton_Registrar");
+boton_registrar.addEventListener("click",function(){
+    const nombre= document.querySelector("#nombre").value;
+    if(nombre.lenght<3){ 
+        alert("El nombre debe tener al menos 3 caracteres");
+        return;
+    }
+    const apellido= document.querySelector("#apellido").value;
+    
+    const rol= document.querySelector("#rol").value;
+    const cliente= new ClienteModelo(nombre, apellido, rol);
+    console.log(cliente);
+
 });
+
+/* una funcion callback es una funcion que se pasa como argumento a otra funcion y se ejecuta despues de que la funcion principal ha terminado su ejecucion. */
